21561b998d13ad63aab82cdaebfa2d28e4dfc1df
Zarnain-Zahra <94491172+Zarnain-Zahra@users.noreply.github.com>
2021-11-17 18:02:23 +0500

    Add files via upload

---
+++ Assignment3.dart
@@ -0,0 +1,25 @@
+import "dart:io";
+void main(){
+
+// Question no.01 (part1)
+
+  // String data= "DART COURSE";
+  // print(data[0].toUpperCase()+data.substring(1).toLowerCase());
+
+// Q1 (part2)
+
+// String data= "dart course";
+// print(data.toUpperCase());
+
+// Q1 (part3)
+
+// String data= "DART COURSE";
+// print(data.toLowerCase());
+
+
+// Question 2
+
+stdout.write("Enter Number");
+int? a=int.parse(stdin.readLineSync()!);
+print(a+5*(10/2));
+}
---
+++ class.dart
@@ -0,0 +1,83 @@
+// STRING METHODS.
+
+void main(){
+//   String data= "ZAHRA QURESHI";
+//   print(data.toLowerCase());
+
+
+
+// String data= "zahra qureshi";
+// print(data.toUpperCase());
+
+// String data= "Zahra Qureshi";
+// var v1= data.split('r');
+// print(v1);
+
+// String data= "Zahra Qureshi";
+// String d2= data.substring(4,10);
+//   print(d2);
+// var v2= data.split('r');
+// print(v2);
+
+// String data= "Zahra Qureshi";
+// String d3= data.substring(6);
+// print(d3);
+
+// String data= "Zahray Ali";
+// String d4= data.substring(6);
+// print(d4);
+
+// String data= "Zahray Ali";
+// var v2= data.split('l');
+// print(v2);
+
+// int a=2;
+// if(++a==3){
+//   print('A is Equal');
+
+// int a=2;
+// if(a++==3){
+//   print(A is equal);
+// }
+
+// String data= "Zahra";
+// String z= data.toUpperCase()+data.substring(2).toLowerCase();
+// print (z);
+
+// String data= "Zahrey";
+// int d= data.length;
+// print(d);
+// print(data[5]);
+
+// String data= "Saif";
+// bool st= data.startsWith('s');
+// print(st);
+
+// String data= "Saif";
+// bool sa= data.startsWith('S');
+// print(sa);
+
+// String data= "Saif";
+// bool sz= data.startsWith('f');
+// print(sz);
+
+// String data= "Naina";
+// bool na= data.endsWith('a');
+// print(na);
+
+// String data= "Saif ";
+// bool ss= data.endsWith(' ');
+// print(ss);
+
+// String data= "Naina";
+// String data2= "Zebr";
+// print(data.compareTo(data2));
+
+// String data="Zehra";
+// int z= data.lastIndexOf('h');
+// print(z);
+
+String data="Hyderabad";
+int f= data.codeUnitAt(2);
+print(f);
+}
